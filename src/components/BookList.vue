3ddeb3faf4e8d56a91e6f3ce88befa320098b153
maryam0704 <2195265@edu.vaniercollege.qc.ca>
2023-08-31 18:35:54 -0400

    adding component

---
+++ src/components/BookList.vue
@@ -0,0 +1,60 @@
+
+
+<template>
+    <div id="app">
+        <h1>Library App</h1>
+        <div v-for="book in books" :key="book._id">
+            <h2>{{ book.title }}</h2>
+            <p>{{ book.author }}</p>
+            <p v-if="book.isReserved">Reserved by: {{ book.reservedBy }}</p>
+            <p v-else>Available</p>
+            <button @click="reserveBook(book._id)" v-if="!book.isReserved">Reserve</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            books: [],
+        };
+    },
+    methods: {
+        async fetchBooks() {
+            try {
+                const response = await fetch("https://reimagined-goldfish-4j7g454xggrx257px-3000.app.github.dev/books");
+                const data = await response.json();
+                this.books = data;
+            } catch (error) {
+                console.error("An error occurred while fetching books:", error);
+            }
+        },
+        async reserveBook(bookId) {
+            try {
+                const response = await fetch(`https://reimagined-goldfish-4j7g454xggrx257px-3000.app.github.dev/books/${bookId}/reserve`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                   //need to make it work with the user name from database
+                    body: JSON.stringify({ reservedBy: "User123" }), 
+                });
+
+                if (response.status === 200) {
+                    console.log("Book reserved successfully");
+                    this.fetchBooks(); 
+                }
+            } catch (error) {
+                console.error("An error occurred while reserving the book:", error);
+            }
+        },
+    },
+    mounted() {
+        this.fetchBooks();
+    },
+};
+</script>
+
+
+
