13c88ddf1f129a3a014ec507823f159f7ce2774c
maryam0704 <2195265@edu.vaniercollege.qc.ca>
2023-09-03 22:39:41 -0400

    changes from login store

--- src/components/AdminPage.vue
+++ src/components/AdminPage.vue
@@ -1,23 +1,29 @@
+
 <template>
-      <div v-if="$userStore.isAuthenticated && $userStore.authenticatedUser === 'admin'">
-        <h1>Welcome, Admin!</h1>
-        <form @submit.prevent="addBook">
-            <label for="title">Book Title:</label>
-            <input v-model="newBook.title" id="title" required />
+    <div>
+        <p v-if="store.isAuthenticated && store.isAdminUser">
+            Authenticated User: {{ store.authenticatedUser }}
+        </p>
+        <div v-if="store.isAuthenticated && store.isAdminUser">
+            <h1>Welcome, Admin!</h1>
+            <form @submit.prevent="addBook">
+                <label for="title">Book Title:</label>
+                <input v-model="newBook.title" id="title" required />
 
-            <label for="author">Author:</label>
-            <input v-model="newBook.author" id="author" required />
+                <label for="author">Author:</label>
+                <input v-model="newBook.author" id="author" required />
 
-            <label for="genre">Genre:</label>
-            <input v-model="newBook.genre" id="genre" required />
-                 <button>Add New Book</button>
+                <label for="genre">Genre:</label>
+                <input v-model="newBook.genre" id="genre" required />
 
-      </form>
-           <button @click="logout">Logout</button>
-      </div>
-      <div v-else>
-        <p>Please log in as an admin.</p>
-      </div>
+                <button>Add New Book</button>
+            </form>
+            <button @click="logout">Logout</button>
+        </div>
+        <div v-else>
+            <p>Please log in as an admin.</p>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -25,76 +31,66 @@ import { useloginStore } from "@/store/loginStore";
 
 export default {
     name: "AdminPage",
-   setup() {
-        const $loginStore = useloginStore();
+    setup() {
+        const store = useloginStore();
         const newBook = {
             title: "",
             author: "",
             genre: "",
         };
-    const addBook = async () => {
+
+        const addBook = async () => {
             try {
-                // if (
+                const response = await fetch(
+                    "https://reimagined-goldfish-4j7g454xggrx257px-3000.app.github.dev/books",
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(newBook),
+                    }
+                );
 
-                // this.newBook.title &&
-                //     this.newBook.author &&
-                //     this.newBook.genre
-
-                //       ) {
-
-                // this.newBook.title = " ";
-                // this.newBook.author = " ";
-                // this.newBook.genre = " ";
-                //   }
-                const response = await fetch('https://reimagined-goldfish-4j7g454xggrx257px-3000.app.github.dev/books', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(newBook),
-                });
                 if (!response.ok) {
-                    throw new Error('Network response was not ok');
+                    throw new Error("Network response was not ok");
                 }
-                // const data = await response.json();
-             newBook.title = " ";
-              newBook.author = " ";
-               newBook.genre = " ";
-                // this.$emit("new book added");
 
-               const data = await response.json();
+                newBook.title = "";
+                newBook.author = "";
+                newBook.genre = "";
+
+                const data = await response.json();
                 console.log(data);
-            //  fetchBooks();
-
-                console.log("Book added successfully");
-                 fetchBooks();
+                fetchBooks();
             } catch (error) {
                 console.error(error);
             }
-          
         };
 
         const fetchBooks = async () => {
             try {
-                 const response = await fetch('https://reimagined-goldfish-4j7g454xggrx257px-3000.app.github.dev/books');
+                const response = await fetch(
+                    "https://reimagined-goldfish-4j7g454xggrx257px-3000.app.github.dev/books"
+                );
+
                 if (!response.ok) {
-                    throw new Error('Network response was not ok');
+                    throw new Error("Network response was not ok");
                 }
+
                 const data = await response.json();
-             
-                this.$data.books = data;
-                 console.log(data);
+                console.log(data);
             } catch (error) {
                 console.error(error);
             }
         };
 
         const logout = () => {
-            $loginStore.logout();
+            store.logout();
         };
 
         return {
-              $userStore: $loginStore,
+            store,
             newBook,
             addBook,
             fetchBooks,
