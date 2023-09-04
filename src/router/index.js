8da13fa7d89fdcaab1487beb4b42e6a4c9872c87
maryam0704 <2195265@edu.vaniercollege.qc.ca>
2023-09-03 22:46:02 -0400

    minor changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,11 @@
-// import Vue from "vue";
-// import VueRouter from "vue-router";
+
 import { createRouter, createWebHistory } from "vue-router";
 import MainView from "@/views/MainView.vue";
 import UserPageView from "@/views/UserPageView.vue";
 import AdminPageView from "@/views/AdminPageView.vue";
 import UserOne from "@/views/UserOne.vue";
 
-// Vue.use(VueRouter);
+
 
 const routes = [
   { path: "/", component: MainView },
