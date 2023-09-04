f5076c20434513d75b7965d250fa8f06314510cc
maryam0704 <2195265@edu.vaniercollege.qc.ca>
2023-09-03 22:47:12 -0400

    added  userlist and store to admin page View

--- src/views/AdminPageView.vue
+++ src/views/AdminPageView.vue
@@ -1,17 +1,38 @@
 
 <template>
     <div>
+         <UserList :allUsers="allUsers" @userSelected="loginUser" />
         <AdminPage />
     </div>
+        <!-- <UserList :allUsers="allUsers" @userSelected="loginUser" /> -->
+
 </template>
 
 <script>
+
 import AdminPage from "@/components/AdminPage.vue";
+import { useloginStore } from "@/store/loginStore";
+import UserList from "@/components/UserList.vue";
+
 
 export default {
     name: "AdminPageView",
     components: {
         AdminPage,
+        UserList,
+    },
+    setup() {
+        const allUsers = ["admin", "userOne"];
+        const loginStore = useloginStore();
+
+        const loginUser = (user) => {
+            loginStore.authenticate(user);
+        };
+        return {
+            allUsers,
+            loginUser,
+            loginStore,
+        };
     },
 };
 </script>
