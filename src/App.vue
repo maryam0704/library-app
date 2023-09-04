4c589b2fa85697d14705dc3206372888dd2fce7b
maryam0704 <2195265@edu.vaniercollege.qc.ca>
2023-09-03 22:45:33 -0400

    added a new router link

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
     <header>
       <nav>
         <router-link to="/">Home</router-link>
-  <router-link to="/user">User Page</router-link>
-  <router-link to="/admin">Admin Page</router-link>
-
+        <router-link to="/user">User Page</router-link>
+        <router-link to="/admin">Admin Page</router-link>
+        <router-link to="/userOne">User One</router-link>
       </nav>
     </header>
 
@@ -15,79 +15,29 @@
       <router-view></router-view>
     </main>
 
-
-  
-    <UserList :allUsers="allUsers" @userSelected="loginUser" />
-     <!-- <AdminPage />  -->
-    <!-- <UserOne />
-    <UserTwo />
-    <Guest /> -->
   </div>
 </template>
 
 <script>
-import { useloginStore } from "@/store/loginStore";
-import UserList from "@/components/UserList.vue";
-// import AdminPage from "@/components/AdminPage.vue";
-// import UserOne from "@/components/UserOne.vue";
-// import UserTwo from "@/components/UserTwo.vue";
-// import Guest from "@/components/Guest.vue";
+// import UserList from "@/components/UserList.vue";
 
 export default {
   name: "App",
-  components: {
-    UserList,
-    // AdminPage,    // UserOne,
-    // UserTwo,
-    // Guest,
-  },
-  setup() {
-    const allUsers = ["admin", "userOne", "userTwo"];
-    const loginStore = useloginStore();
+  // components: {
+  //   UserList,
+  // },
+//   setup() {
+//     const allUsers = ["admin", "userOne"];
+//     const loginStore = useloginStore();
 
-    const loginUser = (user) => {
-      loginStore.authenticate(user);
-    };
-    return {
-      allUsers,
-      loginUser,
-    };
-  },
+//     const loginUser = (user) => {
+//       loginStore.authenticate(user);
+//     };
+//     return {
+//       allUsers,
+//       loginUser,
+//       loginStore, 
+//     };
+//   },
 };
 </script>
-
-<style>
-* {
-  box-sizing: border-box;
-}
-
-body {
-  margin: 0;
-  padding: 0;
-  font-family: sans-serif;
-}
-
-header {
-  background-color: #333;
-  color: #fff;
-  padding: 1rem;
-}
-
-header nav a {
-  color: #fff;
-  text-decoration: none;
-  margin-right: 1rem;
-}
-
-main {
-  padding: 1rem;
-}
-
-h1 {
-  margin: 0;
-}
-
-h2 {
-  margin: 0;
-}
-</style>
