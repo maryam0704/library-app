e47007d28056a5e2f256c516458fe90b28c91eae
maryam0704 <2195265@edu.vaniercollege.qc.ca>
2023-09-03 22:58:37 -0400

    users displayed as buttons instaed of list

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -2,11 +2,19 @@
 <template>
     <div>
         <label for="user">Select a user:</label>
-        <select v-model="selectedUser" id="user">
+        <!-- <select v-model="selectedUser" id="user">
             <option v-for="user in allUsers" :key="user">{{ user }}</option>
         </select>
       
-        <button @click="selectUser">submit</button>
+        <button @click="selectUser">submit</button> -->
+          <button
+              v-for="user in allUsers"
+              :key="user"
+              @click="selectUser(user)"
+              :class="{ active: user === selectedUser }"
+            >
+              {{ user }}
+            </button>
     </div>
 </template>
 
@@ -21,9 +29,10 @@ export default {
         };
     },
     methods: {
-        selectUser() {
+        selectUser(user) {
             this.$emit("userSelected", this.selectedUser);
             console.log("Selected user:", this.selectedUser);
+              this.selectedUser = user;
         },
     },
   
