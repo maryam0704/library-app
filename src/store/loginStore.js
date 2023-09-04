88439366995aeeddd16d02b3dc5b7aad9c1f3780
maryam0704 <2195265@edu.vaniercollege.qc.ca>
2023-09-03 22:51:00 -0400

    login store is now fetching data from database and added a nw function is admin

--- src/store/loginStore.js
+++ src/store/loginStore.js
@@ -4,20 +4,53 @@ export const useloginStore = defineStore("loginStore", {
   state: () => ({
     authenticatedUser: "",
     isAuthenticated: false,
+    isAdmin: false,
   }),
   actions: {
-    authenticate(user) {
-      if (user === "admin" || user === "userOne" || user === "userTwo") {
-        this.authenticatedUser = user;
-        this.isAuthenticated = true;
-      } else {
+    async authenticate(user) {
+      try {
+        const response = await fetch(
+          `https://reimagined-goldfish-4j7g454xggrx257px-3000.app.github.dev/api/users/${user}`
+        );
+
+      
+
+        if (response.ok) {
+          this.authenticatedUser = user;
+          this.isAuthenticated = true;
+
+          if (user === "admin") {
+            this.isAdmin = true;
+          } else if (user === "userOne") {
+            this.isAdmin = false;
+          }
+        }
+        else
+        {
+        
+          console.error("Authentication failed: Invalid username ");
+          this.authenticatedUser = "";
+          this.isAuthenticated = false;
+          this.isAdmin = false;
+        }
+       }
+      catch (error) {
+        console.error("Authentication failed:", error);
         this.authenticatedUser = "";
         this.isAuthenticated = false;
+        this.isAdmin = false;
+        throw error;
       }
     },
+        
+      
     logout() {
       this.authenticatedUser = "";
       this.isAuthenticated = false;
+      this.isAdmin = false;
     },
+  },
+  getters: {
+    isAdminUser: (state) => state.isAdmin || false,
   },
 });
