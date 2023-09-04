99873dfa8727791618917e5cefdde4b397447c43
maryam0704 <2195265@edu.vaniercollege.qc.ca>
2023-09-03 22:56:37 -0400

    admin can see all the borrowed books

--- src/components/UserBorrowedBooks.vue
+++ src/components/UserBorrowedBooks.vue
@@ -8,7 +8,12 @@
             </li>
         </ul>
 
-       
+        <h2 v-if="isAdmin">all the borrowed books</h2>
+        <ul v-if="isAdmin">
+            <li v-for="book in adminBorrowedBooks" :key="book.title">
+                {{ book.title }} by {{ book.reservedBy }}
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -19,7 +24,8 @@ export default {
     data() {
         return {
             borrowedBooks: [],
-          
+            adminBorrowedBooks: [],
+            isAdmin: false,
         };
     },
     methods: {
@@ -42,13 +48,34 @@ export default {
             }
         },
 
-        
+        async fetchAdminBorrowedBooks() {
+            try {
+                const loginStore = useloginStore();
+
+                if (loginStore.isAdmin) {
+                    const response = await fetch(
+                        "https://reimagined-goldfish-4j7g454xggrx257px-3000.app.github.dev/admin/borrowedbooks"
+                    );
+
+                    if (response.ok) {
+                        const data = await response.json();
+                        this.adminBorrowedBooks = data;
+                    } else {
+                        console.error("Failed to fetch admin borrowed books");
+                    }
+                } else {
+                    console.log("You must be an admin to access this feature.");
+                }
+            } catch (error) {
+                console.error("An error occurred while fetching admin borrowed books:", error);
+            }
+        },
     },
     mounted() {
         const loginStore = useloginStore();
-       
+        this.isAdmin = loginStore.isAdmin;
         this.fetchBorrowedBooks();
-        
+        this.fetchAdminBorrowedBooks();
     },
 };
 </script>
