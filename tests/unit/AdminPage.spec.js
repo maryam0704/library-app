6f78b51c2aee18a017f10403363c5858c6738891
maryam0704 <2195265@edu.vaniercollege.qc.ca>
2023-09-04 23:52:02 -0400

    failed tests

--- tests/unit/AdminPage.spec.js
+++ tests/unit/AdminPage.spec.js
@@ -1,5 +1,5 @@
-import { shallowMount } from "@vue/test-utils";
-import AdminPage from "@/components/AdminPage.vue";
+// import { shallowMount } from "@vue/test-utils";
+// import AdminPage from "@/components/AdminPage.vue";
 
 // const mockStore = {
 //   isAuthenticated: true,
